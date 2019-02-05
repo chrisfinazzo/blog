6c4e40cedd46090b7007c2b29b584dec5f5d325f
chrisfinazzo <chris.finazzo@gmail.com>
2019-02-05 12:46:01 -0500

    Delete the gulpfile

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-const gulp = require("gulp");
-const shell = require("gulp-shell");
-
-gulp.task("build", shell.task("bundle exec jekyll doctor && time bundle exec jekyll build --incremental"));
-
-gulp.task("serve", shell.task("bundle exec jekyll doctor && time bundle exec jekyll serve --incremental"));
-
-gulp.task("deploy", shell.task("git push origin master && rsync -crz --exclude-from='rsync-exclude.txt' --delete _site/ cfinazzo1@chrisfinazzo.com:/home/cfinazzo1/chrisfinazzo.com"));
