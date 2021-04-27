ba18a30982e07c7d6d61a702b429641a3e38d512
chrisfinazzo <chris.finazzo@gmail.com>
2021-04-27 15:01:04 -0400

    Delete the service worker, fix the homepage loop

--- loadWorker.js
+++
@@ -1,9 +0,0 @@
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js')
-    .then(function (registration) {
-      console.log('Success!', registration.scope);
-    })
-    .catch(function (error) {
-      console.error('Failure!', error);
-    });
-}
